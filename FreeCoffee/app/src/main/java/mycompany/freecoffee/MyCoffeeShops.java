6414f1bca196e59af83fa0d086f72de599a75e14
daniel0001 <daniel.j.halliday@gmail.com>
2017-01-25 16:47:58 +0000

    First commit

---
+++ FreeCoffee/app/src/main/java/mycompany/freecoffee/MyCoffeeShops.java
@@ -0,0 +1,17 @@
+package mycompany.freecoffee;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class MyCoffeeShops extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_coffee_shops);
+    }
+
+
+}
