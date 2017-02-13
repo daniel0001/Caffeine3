7f74d3e58b4541996480748de91571e3cd742e6f
daniel0001 <daniel.j.halliday@gmail.com>
2017-02-13 16:01:14 +0000

    Improved egister Activity by adding in check that all Edit text fields are complete

--- LoginRegisterUser/app/src/main/java/practice/loginregisteruser/RegisterActivity.java
+++ LoginRegisterUser/app/src/main/java/practice/loginregisteruser/RegisterActivity.java
@@ -1,5 +1,6 @@
 package practice.loginregisteruser;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
@@ -8,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -15,6 +17,8 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.Map;
 
 /**
  * Created by Daniel on 06/02/2017.
@@ -38,13 +42,24 @@ public class RegisterActivity extends AppCompatActivity {
         bRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 final String name = etName.getText().toString();
                 final String username = etUsername.getText().toString();
                 final String password = etPassword.getText().toString();
                 final String country = etCountry.getText().toString();
                 final String city = etTownCity.getText().toString();
                 final String email = etEmailAddress.getText().toString();
+
+                final String phoneNum = etPhone.getText().toString();
+
+                // Check that all of the edit text fields have been completed
+                if (name.length() == 0 || username.length() == 0 || password.length() == 0 || country.length() == 0 || city.length() == 0 || email.length() == 0 || phoneNum.length() == 0 ){
+                    Toast.makeText(RegisterActivity.this, "Please complete all of the user info.", Toast.LENGTH_LONG).show();
+                    return;         // if not completed return to start
+                }
+
                 final int phone = Integer.parseInt(etPhone.getText().toString());
+
 
                 Response.Listener<String> responseListener = new Response.Listener<String>() {
 
