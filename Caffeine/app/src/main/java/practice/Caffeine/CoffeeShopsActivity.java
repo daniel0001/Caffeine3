dcce56fabd6312b5063257aefe771871865225af
daniel0001 <daniel.j.halliday@gmail.com>
2017-02-18 15:47:28 +0000

    Building an internal SQL database to track user data
    why still referring to loginregisteruser for data storage?

--- Caffeine/app/src/main/java/practice/Caffeine/CoffeeShopsActivity.java
+++ Caffeine/app/src/main/java/practice/Caffeine/CoffeeShopsActivity.java
@@ -2,17 +2,68 @@ package practice.Caffeine;
 
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class CoffeeShopsActivity extends AppCompatActivity {
      @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_coffee_shops);
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.activity_coffee_shops);
+
+        // Instantiate new local SQL DB = UserDetails
+         UserDetailsContract.UserDetailsDBHelper mDbHelper = new UserDetailsContract.UserDetailsDBHelper(this);
+         // Gets the data repository in write mode
+         SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+// Define a projection that specifies which columns from the database
+// you will actually use after this query.
+         String[] projection = {
+                 UserDetailsContract.UserDetails._ID,
+                 UserDetailsContract.UserDetails.COLUMN_NAME_USERNAME,
+         };
+
+// Filter results WHERE "title" = 'My Title'
+         String selection = UserDetailsContract.UserDetails.COLUMN_NAME_USERNAME + " = ?";
+         String[] selectionArgs = {"username"};
+
+// How you want the results sorted in the resulting Cursor
+         String sortOrder =
+                 UserDetailsContract.UserDetails.COLUMN_NAME_USERNAME + " DESC";
+
+         Cursor cursor = db.query(
+                 UserDetailsContract.UserDetails.TABLE_NAME,                     // The table to query
+                 projection,                               // The columns to return
+                 selection,                                // The columns for the WHERE clause
+                 selectionArgs,                            // The values for the WHERE clause
+                 null,                                     // don't group the rows
+                 null,                                     // don't filter by row groups
+                 sortOrder                                 // The sort order
+         );
+         List <String>itemIds = new ArrayList<>();
+         while (cursor.moveToNext()) {
+             String itemId = cursor.getString(
+                     cursor.getColumnIndexOrThrow(UserDetailsContract.UserDetails._ID));
+             itemIds.add(itemId);
+         }
+         cursor.close();
+
+         // Display the SQL data in the edit text field test
+         final EditText etUsername = (EditText) findViewById(R.id.etDBTest);
+
+         etUsername.setText(itemIds.get(0));
+
+
+         // Variables passed to this activity from Login
          Intent intent = getIntent();
          final String name = intent.getStringExtra("name");
          final String userID = intent.getStringExtra("userID");
@@ -24,10 +75,11 @@ public class CoffeeShopsActivity extends AppCompatActivity {
          final Button bAddShop = (Button) findViewById(R.id.bAddShop);
 
 
-         bAddShop.setOnClickListener(new View.OnClickListener(){
+         // Button to move to new coffee shop activity with variables
+         bAddShop.setOnClickListener(new View.OnClickListener() {
 
              @Override
-             public void onClick(View v){
+             public void onClick(View v) {
                  Intent intent = new Intent(CoffeeShopsActivity.this, NewCoffeeShop.class);
                  intent.putExtra("username", username);
                  intent.putExtra("userID", userID);
@@ -40,7 +92,7 @@ public class CoffeeShopsActivity extends AppCompatActivity {
              }
          });
 
-    }
+     }
 
 
 
--- Caffeine/app/src/main/java/practice/Caffeine/LoginActivity.java
+++ Caffeine/app/src/main/java/practice/Caffeine/LoginActivity.java
@@ -1,6 +1,8 @@
 package practice.Caffeine;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -26,6 +28,11 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // Instantiate new local SQL DB = UserDetails
+        UserDetailsContract.UserDetailsDBHelper mDbHelper = new UserDetailsContract.UserDetailsDBHelper(this);
+        // Gets the data repository in write mode
+       final SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
 
         final EditText etUsername = (EditText) findViewById(R.id.etUsername);
         final EditText etPassword = (EditText) findViewById(R.id.etPassword);
@@ -39,6 +46,7 @@ public class LoginActivity extends AppCompatActivity {
                 LoginActivity.this.startActivity(registerIntent);
             }
         });
+
 
         // listen to the button being clicked
         bLogin.setOnClickListener(new View.OnClickListener() {
@@ -83,6 +91,24 @@ public class LoginActivity extends AppCompatActivity {
                                 intent.putExtra("country", country);
                                 intent.putExtra("email", email);
 
+
+                                // Insert User data from JSON Response into DB
+                                // Create a new map of values, where column names are the keys
+                                ContentValues values = new ContentValues();
+                                values.put(UserDetailsContract.UserDetails.COLUMN_NAME_USERNAME, username);
+                                values.put(UserDetailsContract.UserDetails.COLUMN_NAME_USERID, userID);
+                                values.put(UserDetailsContract.UserDetails.COLUMN_NAME_NAME , name);
+                                values.put(UserDetailsContract.UserDetails.COLUMN_NAME_PASSWORD, password);
+                                values.put(UserDetailsContract.UserDetails.COLUMN_NAME_PHONE, phone);
+                                values.put(UserDetailsContract.UserDetails.COLUMN_NAME_CITY, city);
+                                values.put(UserDetailsContract.UserDetails.COLUMN_NAME_COUNTRY, country);
+                                values.put(UserDetailsContract.UserDetails.COLUMN_NAME_EMAIL, email);
+
+
+                                // Insert the new row
+                                db.insert(UserDetailsContract.UserDetails.TABLE_NAME, null, values);
+
+
                                 //Open UserAreaActivity
                                 LoginActivity.this.startActivity(intent);
                             } else {
@@ -107,4 +133,7 @@ public class LoginActivity extends AppCompatActivity {
 
 
     }
-}
+
+    }
+
+
